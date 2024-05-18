68e1df9589a5d8ce57eaec88ac12e0e5278e6068
Christopher4113 <ch.lam1328@gmail.com>
2024-05-18 00:07:17 -0400

    AI related changes

--- game.py
+++ game.py
@@ -1,1 +1,37 @@
-print("hello world")
+import neat.config
+import pygame
+import neat
+import time
+import os
+import random
+pygame.font.init()
+
+
+
+
+
+
+
+
+
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path)
+
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    #winner = p.run(main,50)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config_feedforward.txt")
+    run(config_path)
